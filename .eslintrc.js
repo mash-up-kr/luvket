9e48f2d657784d90592fa69093515e5e954ffed5
chayeoi <chayeoikeem@gmail.com>
2019-11-16 16:37:09 +0900

    chore: eslint 관련 설정 추가
    - airbnb 스타일 가이드 적용
    - hooks 관련 eslint rule 추가
    
    Co-authored-by: yurim22 <dek1313@naver.com>
    Co-authored-by: jus0k <jusung.design@gmail.com>
    Signed-off-by: chayeoi <chayeoikeem@gmail.com>

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'reract-hooks',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+};
