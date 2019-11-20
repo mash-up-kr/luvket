e6de763ba4b57d2d98758674306089220bda9bf8
chayeoi <chayeoikeem@gmail.com>
2019-11-20 09:49:01 +0900

    chore: styllint 설정 적용
    stylelint-config-standard 설정을 적용함
    stylelint 수행을 위한 스크립트를 추가함
    prettier와 stylelint 설정을 통합함
    
    Fixes #3
    
    Signed-off-by: chayeoi <chayeoikeem@gmail.com>

---
+++ .stylelintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-prettier/recommended',
+  ],
+  rules: {
+    'declaration-empty-line-before': null,
+    'no-empty-source': null,
+    'no-missing-end-of-source-newline': null,
+  },
+};
