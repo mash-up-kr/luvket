4dd048725ec14b40046b80f8e3ba6ac64fd333d7
chayeoi <chayeoikeem@gmail.com>
2019-11-24 00:06:57 +0900

    feat: 전역적으로 사용되는 theme과 glboal 스타일을 추가 (#13)
    - `ThemeProvider`를 통해 theme을 전역적으로 사용할 수 있도록 함
    - `Global`를 통해 Reset CSS 코드를 추가함
    
    Co-authored-by: jus0k <jusung.design@gmail.com>
    Signed-off-by: chayeoi <chayeoikeem@gmail.com>

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,4 +1,6 @@
+import { Global } from '@emotion/core';
 import { ConnectedRouter } from 'connected-react-router';
+import { ThemeProvider } from 'emotion-theming';
 import { History } from 'history';
 import React, { Suspense } from 'react';
 import { Route, Switch } from 'react-router-dom';
@@ -7,6 +9,7 @@ import { RouteEnum } from '../constants';
 import { Spinner } from './components';
 import Home from './Home';
 import NotFound from './NotFound';
+import { global, theme } from './styles';
 
 interface Props {
   readonly history: History;
@@ -14,12 +17,15 @@ interface Props {
 
 const App: React.FC<Props> = ({ history }: React.PropsWithChildren<Props>) => (
   <ConnectedRouter history={history}>
-    <Suspense fallback={<Spinner />}>
-      <Switch>
-        <Route exact path={RouteEnum.Home} component={Home} />
-        <Route component={NotFound} />
-      </Switch>
-    </Suspense>
+    <ThemeProvider theme={theme}>
+      <Global styles={global} />
+      <Suspense fallback={<Spinner />}>
+        <Switch>
+          <Route exact path={RouteEnum.Home} component={Home} />
+          <Route component={NotFound} />
+        </Switch>
+      </Suspense>
+    </ThemeProvider>
   </ConnectedRouter>
 );
 
---
+++ src/views/styles/theme.ts
@@ -0,0 +1,7 @@
+import Theme from '../../models/Theme';
+
+const theme: Theme = {
+  palette: {},
+};
+
+export default theme;
