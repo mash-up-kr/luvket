40c4bbf1d8df4a46ee16608e78ceb134777ce596
chayeoi <chayeoikeem@gmail.com>
2019-11-25 18:30:29 +0900

    chore: 소스 코드에 JSX Pragma를 사용하지 않더라도 JSX 표현식에 `jsx`를 사용하도록 함 (#38)
     - ESLint 규칙에 react/react-in-jsx-scope을 `off`로 설정
     - CRA ESLint 규칙을 덮어씌우기 위해 EXTEND_ESLINT 환경변수 추가
     - JSX Pragma를 사용하던 코드를 제거
    
    Signed-off-by: chayeoi <chayeoikeem@gmail.com>

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const Home: React.FC = () => <div>Home</div>;
 
 export default Home;
--- src/views/components/Spinner/Spinner.tsx
+++ src/views/components/Spinner/Spinner.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const Spinner: React.FC = () => <div>Loading...</div>;
 
 export default Spinner;
