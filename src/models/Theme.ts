d3b3c52ee8c7c7da3eec7a0e9fc1382c6b436f64
jus0k <jutjd115@gmail.com>
2019-12-05 21:18:05 +0900

    feat: 초대 화면 제목 영역 추가 (#40)
    - 화면 내 제목 및 설명 영역 추가
    - Layout 공용 컴포넌트 추가
    - typography theme 정의 및 css 작성
    
    Signed-off-by: jus0k <jutjd115@gmail.com>

--- src/models/Theme.ts
+++ src/models/Theme.ts
@@ -2,9 +2,18 @@ export default interface Theme {
   readonly palette: {
     gray: {
       100: string;
+      500: string;
     };
   };
   readonly typography: {
+    title: {
+      fontSize: string;
+      fontWeight: number;
+    };
+    description: {
+      fontSize: string;
+      lineHeight: number;
+    };
     button: {
       fontSize: string;
     };
--- src/views/components/index.ts
+++ src/views/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Button } from './Button';
 export { default as Header } from './Header';
+export { default as Layout } from './Layout';
 export { default as Spinner } from './Spinner';
