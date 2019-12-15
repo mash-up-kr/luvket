15782ce48cb93c883338cbb9821ebd8339b1694c
jus0k <jutjd115@gmail.com>
2019-12-15 20:09:36 +0900

    feat: 디자인 변경에 따른 Button, Header 공용 컴포넌트 스타일 변경
    - Button 컴포넌트 텍스트 사이즈 button(14px)에서 caption(12px)로 변경 및 padding 추가
    - Header 컴포넌트 하단 라인 색상 gray-300에서 gray-200으로 변경 및 padding 변경
    
    Signed-off-by: jus0k <jutjd115@gmail.com>

--- src/views/components/Header/Header.tsx
+++ src/views/components/Header/Header.tsx
@@ -8,12 +8,12 @@ const s = {
   wrapper: (theme: Theme): SerializedStyles => css`
     width: 100%;
     height: 50px;
-    padding: 0 1rem;
+    padding: 10px 5px;
     display: flex;
     flex-direction: row;
     align-items: center;
     justify-content: space-between;
-    border-bottom: 1px solid ${theme.palette.gray[300]};
+    border-bottom: 1px solid ${theme.palette.gray[200]};
   `,
 };
 
