33fc66dcc2f357178e733934ad81426ac025162f
chayeoi <chayeoikeem@gmail.com>
2020-01-08 02:27:54 +0900

    chore(stylelint): stylelint에서 카멜케이스 함수 이름 사용 시 에러를 발생시키는 규칙을 off함
    css 내에서 `pxToRem`과 같이 카멜케이스로 된 함수 이름을 사용 시
    에러를 발생시키던 규칙을 off하기 위한 규칙을 추가
    
    Signed-off-by: chayeoi <chayeoikeem@gmail.com>

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -7,5 +7,8 @@ module.exports = {
     'declaration-empty-line-before': null,
     'no-empty-source': null,
     'no-missing-end-of-source-newline': null,
+    'function-name-case': ['lower', {
+      ignoreFunctions: [/^\${/],
+    }],
   },
 };
